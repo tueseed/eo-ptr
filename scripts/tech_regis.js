0d944b4c8b57e57c37e2f55b4937f58840400cf9
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-18 17:00:49 +0700

    dsadsad

--- scripts/tech_regis.js
+++ scripts/tech_regis.js
@@ -5,11 +5,13 @@ $('#head_text').html('EO_REPORT')
 window.onload = function(e) {
                                 liff.init(function(data) {
                                                             localStorage.setItem('userId', data.context.userId)
-                                                            alert(data.context.userId)
+                                                            //alert(data.context.userId)
                                 liff
                                     .getProfile()
                                     .then(function(profile){
                                                             alert(profile)
+                                                            $('#uNameInput').html(profile.displayName)
+                                                            $('#profileImage').attr('src',profile.pictureUrl)
                                                             }
                                         )
                                                             //tech.orderByChild('uid').equalTo(data.context.userId).once('value',function (snapshot){
