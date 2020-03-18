ade7745666f15a51f3ba3dabd0df5f3707adcb70
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-18 16:50:21 +0700

    dasdasd

--- scripts/tech_regis.js
+++ scripts/tech_regis.js
@@ -1,15 +1,22 @@
+var tech = firebase.database().ref('tech')
+
 $('#head_text').html('EO_REPORT')
 
 window.onload = function(e) {
                                 liff.init(function(data) {
-                                                            localStorage.setItem('userId', data.context.userId);
+                                                            localStorage.setItem('userId', data.context.userId)
                                                             alert(data.context.userId)
-                                                            // liff
-                                                            //     .getProfile()
-                                                            //     .then(function(profile){
-                                                            //                             $("#dear_title").text("เรียน คุณ "+profile.displayName);
-                                                            //                             }
-                                                            //         )
+                                liff
+                                    .getProfile()
+                                    .then(function(profile){
+                                                            alert(profile)
+                                                            }
+                                        )
+                                                            //tech.orderByChild('uid').equalTo(data.context.userId).once('value',function (snapshot){
+                                                                                                                                                    // console.log(snapshot.val())
+                                                                                                                                                    // })
                                                         }
                                         )
                             }
+
+
