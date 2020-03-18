fdda21cc733014d1fd84b77463a8e53b78a9d541
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-18 15:06:19 +0700

    adfadsd

--- scripts/tech_regis.js
+++ scripts/tech_regis.js
@@ -1,1 +1,15 @@
 $('#head_text').html('EO_REPORT')
+
+window.onload = function(e) {
+                                liff.init(function(data) {
+                                                            localStorage.setItem('userId', data.context.userId);
+                                                            alert(data.context.userId)
+                                                            // liff
+                                                            //     .getProfile()
+                                                            //     .then(function(profile){
+                                                            //                             $("#dear_title").text("เรียน คุณ "+profile.displayName);
+                                                            //                             }
+                                                            //         )
+                                                        }
+                                        )
+                            }
