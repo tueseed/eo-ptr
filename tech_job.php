9d60f04c625f9c42fa0bf4c589ac842c5ceb78d6
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-19 14:19:59 +0700

    asfdasf

---
+++ tech_job.php
@@ -0,0 +1,9 @@
+<div class="row text-center">
+    <div class="col-sm-12">
+        <div class="form-group text-center">
+            <h4 class="font-weight-bold">ยินดีต้อนรับ<br/><div id="uNameInput"></div></h4>
+            <img id="profileImage" class="shadow-sm" style="width:100px;height:100px;border-radius:50px 50px;"/>
+            <input type="hidden" id="uIdInput" name="uIdInput">
+        </div>
+    </div>
+</div>
