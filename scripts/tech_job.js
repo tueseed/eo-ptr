9ee299559a5e6aa5902fa75881c9a2c064c327ed
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-03-26 20:17:25 +0700

    fdsfef

--- scripts/tech_job.js
+++ scripts/tech_job.js
@@ -1,25 +1,46 @@
 var tech = firebase.database().ref('tech')
 var fb = firebase.database().ref('event')
 $('#head_text').html('EO_REPORT')
-window.onload = function(e) {
-                                liff.init({liffId:'1653963133-E5eYJn42'}).then(function(data) {
-                                                            localStorage.setItem('userId', data.context.userId)
-                                                            alert(data.context.userId)
-                                                            tech.orderByChild('uid').equalTo(data.context.userId).once('value',function (snapshot){
-                                                                if(snapshot.val() == null)
-                                                                {
-                                                                    window.location.href = 'index.php?action=tech_regis'
-                                                                }
-                                                                })
-                                                            liff
-                                                            .getProfile()
-                                                            .then(function(profile){
-                                                                                    $('#uNameInput').html(profile.displayName)
-                                                                                    $('#profileImage').attr('src',profile.pictureUrl)
-                                                                                    }
-                                                                )
-                                                        }
-                                        )
+// window.onload = function(e) {
+//                                 liff.init(function(data) {
+//                                                             localStorage.setItem('userId', data.context.userId)
+//                                                             tech.orderByChild('uid').equalTo(data.context.userId).once('value',function (snapshot){
+//                                                                 if(snapshot.val() == null)
+//                                                                 {
+//                                                                     window.location.href = 'index.php?action=tech_regis'
+//                                                                 }
+//                                                                 })
+//                                                             liff
+//                                                             .getProfile()
+//                                                             .then(function(profile){
+//                                                                                     $('#uNameInput').html(profile.displayName)
+//                                                                                     $('#profileImage').attr('src',profile.pictureUrl)
+//                                                                                     }
+//                                                                 )
+//                                                         }
+//                                         )
+// }
+window.onload = async function(e) {
+    var init = await liff.init({liffId:'1653963133-E5eYJn42'})
+    var getconText = await liff.getContext()
+    alert(getconText.userId)
+    // liff.init(function(data) {
+    //                             localStorage.setItem('userId', data.context.userId)
+    //                             tech.orderByChild('uid').equalTo(data.context.userId).once('value',function (snapshot){
+    //                                 if(snapshot.val() == null)
+    //                                 {
+    //                                     window.location.href = 'index.php?action=tech_regis'
+    //                                 }
+    //                                 })
+    //                             liff
+    //                             .getProfile()
+    //                             .then(function(profile){
+    //                                                     $('#uNameInput').html(profile.displayName)
+    //                                                     $('#profileImage').attr('src',profile.pictureUrl)
+    //                                                     }
+    //                                 )
+    //                         }
+    //         )
 }
 
 function change_tech_status()
